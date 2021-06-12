5d080d648e27bc5402e0c4cf3172a5c93220ffe5
Arcadio Buelvas <arcadio.buelvas@cision.com>
2021-06-11 19:16:16 -0500

    Handling status codes

---
+++ constants/constantes.js
@@ -0,0 +1,16 @@
+const constants = {
+    USER_SUCCESFULLY_CREATED: 'The User has been created sucessfully ID:',
+    MISSING_REQUESTED_FIELDS: 'Missing required fields: First Name, Last Name or Age',
+    NOT_FOUND: 'Server Not found',
+    USER_NOT_FOUND: 'The user you are looking for does not exits',
+    SUCCESS_DELETE: 'The user was succesfully deleted ID: ',
+    FAILED_DELETE: 'The user you want to delete does not exist: ',
+    SERVER_ERROR: 'Oppps something went wrong. Internal Server Error',
+    WRONG_DATA: 'You are sending not required data or an empty body',
+    SUCCESS_UPDATE: 'The user has been succesfully update ID: '
+};
+
+export default constants;
+
+
+
--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
+import errors from '../utils/errors.js'
+import constants from '../constants/constantes.js'
 
 let users = [];
 
@@ -9,29 +11,63 @@ export const getUsers = (req, res) => {
 export const getUser = (req, res) => {
     const id = req.params.id;
     const foundUser = users.find((user) => user.id === id);
+    if (!foundUser) {
+        throw new errors.NotFound(constants.USER_NOT_FOUND);
+    }
     res.send(foundUser);
 };
 
-export const createUsers = (req, res) => {
-    let user = req.body;
-    const userId = uuidv4();
-    const userWithId = {... user, id: userId};
-    users.push(userWithId);
-    res.send(`The User with name: ${userWithId.firstName} and ID: ${userWithId.id} has been created sucessfully`);
+export const createUsers = (req, res, next) => {
+    let userId = uuidv4();
+    let { firstName, lastName, age } = req.body;
+    if (!firstName || !lastName || !age) {
+        throw new errors.BadRequest(constants.MISSING_REQUESTED_FIELDS);
+    }
+    const user = { firstName, lastName, age };
+    const userWithId = { ...user, id: userId };
+    try {
+        users.push(userWithId);
+        res.json({ message: `${constants.USER_SUCCESFULLY_CREATED} ${userWithId.id}` });
+        res.send();
+    } catch (error) {
+        throw new errors.ServerError(`${constants.SERVER_ERROR}, ${error}`);
+    };
 };
 
 export const deleteUser = (req, res) => {
     const id = req.params.id;
-    users = users.filter((user) => user.id !== id);
-    res.send(`The User with ID: ${id} was succesfully deleted`);
+    let userToBeDeleted = users.find((user) => user.id !== id);
+    if (!userToBeDeleted) {
+        throw new errors.NotFound(constants.FAILED_DELETE);
+    }
+    try {
+        users = users.filter((user) => user.id !== id);
+        res.json({ message: `${constants.SUCCESS_DELETE} ${id}` });
+        res.send();
+    } catch (error) {
+        throw new errors.ServerError(`${constants.SERVER_ERROR}, ${error}`);
+    };
 };
 
 export const updateUser = (req, res) => {
     const id = req.params.id;
-    const user = users.find((user) => user.id == id );
-    const {firstName, lastName, age} = req.body;
-    if(firstName) user.firstName = firstName;
-    if(lastName) user.lastName = lastName;
-    if(age) user.age = age;
-    res.send(`The user with ID: ${id} has been updated succesfully`);
+    const user = users.find((user) => user.id == id);
+    if (!user) {
+        throw new errors.NotFound(constants.USER_NOT_FOUND);
+    }
+    const { firstName, lastName, age } = req.body;
+    if (firstName || lastName || age) {
+        try {
+            if (firstName) user.firstName = firstName;
+            if (lastName) user.lastName = lastName;
+            if (age) user.age = age;
+            res.json({ message: `${constants.SUCCESS_UPDATE} ${id}` });
+            res.send();
+        }
+        catch (error) {
+            throw new errors.ServerError(`${constants.SERVER_ERROR}  ${error}`);
+        }
+    } else {
+        throw new errors.BadRequest(`${constants.WRONG_DATA}`);
+    }
 };
